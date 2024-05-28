c97e87601a43a52c081a5a98402e4793adad691c
waterslinky <waterslinky10@gmail.com>
2024-05-28 16:46:42 -0700

    basic enemy

--- scripts/basic_sword.gd
+++ scripts/basic_sword.gd
@@ -9,9 +9,9 @@ func _process(delta):
 	if(Input.is_action_just_pressed("attack")):
 		active_timer=25
 	
-	if(active_timer):
+	if(active_timer>0):
 		self.visible=true
-		active_timer-=1
+		active_timer-=60*delta
 	else:
 		self.visible=false
 	#position.x=player.position.x
